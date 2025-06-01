11934b9af72bd564a981ff8ea33e54695d008aab
yq2006 <15237235575@163.com>
2025-06-01 16:38:58 +0800

    Update main.cpp

--- untitled5/main.cpp
+++ untitled5/main.cpp
@@ -7,17 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     StartMenu menu;
-   // MainWindow w;
-
-    // QObject::connect(&menu, &StartMenu::startPVP, [&](){
-    //   w.initPVPGame();
-    //     w.show();
-    // });
-
-    // QObject::connect(&menu, &StartMenu::startPVE, [&](){
-    //  w.initPVEGame();
-    //     w.show();
-    // });
 
     menu.show();
     return a.exec();
